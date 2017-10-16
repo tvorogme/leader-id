ac4acf8cb8dae2381c375950fee73a7cb06539c7
Andrew Tvorozhkov <tvorog@tvorog.me>
2017-10-16 16:09:05 +0300

    Uncoment DB update

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ from db.models import create_tables
 if __name__ == "__main__":
     # # Init database
     create_tables()
-    #
-    # from parsers.all import update_all_wapper, update_all
-    #
-    # # Update
-    # update_all()
-    #
-    # from recommender.main import generate_vectors, update_similars
-    #
-    # # Generate vectors
-    # generate_vectors()
-    # update_similars()
-    #
-    # # Generate thread with dataset update
-    # update_all_wapper()
-    #
+
+    from parsers.all import update_all_wapper, update_all
+
+    # Update
+    update_all()
+
+    from recommender.main import generate_vectors, update_similars
+
+    # Generate vectors
+    generate_vectors()
+    update_similars()
+
+    # Generate thread with dataset update
+    update_all_wapper()
+
     from api.main import run_api
 
     run_api()
 
-    # from db.utils import print_status
+    from db.utils import print_status
 
-    # print_status()
+    print_status()
